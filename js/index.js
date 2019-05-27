b5c84b5ce03ea0f70a6cc453630176aa3349b9ad
Jangle <n.zhangeldy@gmail.com>
2019-05-27 09:54:32 +0600

    add index html

---
+++ js/index.js
@@ -0,0 +1,27 @@
+/*document.getElementById("tab-label-1").addEventListener('click', function (event) {
+  console.log(event)
+})*/
+document.addEventListener("DOMContentLoaded", function () {
+  let tabLabel1 = document.getElementById("tab-label-1");
+  let tabLabel2 = document.getElementById("tab-label-2");
+
+  let tabContainer1 = document.getElementById("tab-container-1");
+  let tabContainer2 = document.getElementById("tab-container-2");
+
+  tabLabel1.addEventListener("click", function () {
+    tabLabel1.classList.add("active");
+    tabContainer1.classList.add("active");
+
+    tabLabel2.classList.remove("active");
+    tabContainer2.classList.remove("active");
+  });
+
+  tabLabel2.addEventListener("click", function () {
+    tabLabel2.classList.add("active");
+    tabContainer2.classList.add("active");
+
+    tabLabel1.classList.remove("active");
+    tabContainer1.classList.remove("active");
+  });
+
+});
