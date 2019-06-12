0d03c63409c994571751b3a238c72a1a8bae056b
Jangle <n.zhangeldy@gmail.com>
2019-06-12 09:27:25 +0600

    Урок-5

--- js/index.js
+++ js/index.js
@@ -1,40 +1,51 @@
 /*document.getElementById("tab-label-1").addEventListener('click', function (event) {
   console.log(event)
 })*/
+
 document.addEventListener("DOMContentLoaded", function () {
-    let tabLabel1 = document.getElementById("tab-label-1");
-    let tabLabel2 = document.getElementById("tab-label-2");
 
-    let tabContainer1 = document.getElementById("tab-container-1");
-    let tabContainer2 = document.getElementById("tab-container-2");
+  document.body.addEventListener('click', function (event) {
+    let element = event.target;
+    let headerMenu = element.closest('.header-menu');
+    let accountIcon = element.closest('#account');
+    if (headerMenu === null && accountIcon === null) {
+      document.getElementById("headerMenu").style.visibility = 'hidden';
+    }
 
-    tabLabel1.addEventListener("click", function () {
-        tabLabel1.classList.add("active");
-        tabContainer1.classList.add("active");
+  });
 
-        tabLabel2.classList.remove("active");
-        tabContainer2.classList.remove("active");
-    });
+  let tabLabel1 = document.getElementById("tab-label-1");
+  let tabLabel2 = document.getElementById("tab-label-2");
 
-    tabLabel2.addEventListener("click", function () {
-        tabLabel2.classList.add("active");
-        tabContainer2.classList.add("active");
+  let tabContainer1 = document.getElementById("tab-container-1");
+  let tabContainer2 = document.getElementById("tab-container-2");
 
-        tabLabel1.classList.remove("active");
-        tabContainer1.classList.remove("active");
-    });
+  tabLabel1.addEventListener("click", function () {
+    tabLabel1.classList.add("active");
+    tabContainer1.classList.add("active");
 
-    let headerMenu = document.getElementById("headerMenu");
-    let account = document.getElementById("account");
+    tabLabel2.classList.remove("active");
+    tabContainer2.classList.remove("active");
+  });
 
-    account.addEventListener("click", function () {
-        if (headerMenu.style.visibility === 'visible') {
-            headerMenu.style.visibility = 'hidden';
-        } else {
-            headerMenu.style.visibility = 'visible';
-        }
+  tabLabel2.addEventListener("click", function () {
+    tabLabel2.classList.add("active");
+    tabContainer2.classList.add("active");
 
-    });
+    tabLabel1.classList.remove("active");
+    tabContainer1.classList.remove("active");
+  });
+
+  let headerMenu = document.getElementById("headerMenu");
+  let account = document.getElementById("account");
+
+  account.addEventListener("click", function () {
+    if (headerMenu.style.visibility === 'visible') {
+        headerMenu.style.visibility = 'hidden';
+    } else {
+        headerMenu.style.visibility = 'visible';
+    }
+  });
 
 });
 
